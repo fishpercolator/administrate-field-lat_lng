71a7d464baff21f019cb2e390f2d0c99e59a57dc
Rich Daley <rich@fishpercolator.co.uk>
2016-05-12 12:20:47 +0100

    Add some really basic specs.

--- spec/administrate/field/lat_lng_spec.rb
+++ spec/administrate/field/lat_lng_spec.rb
@@ -2,6 +2,60 @@ require 'spec_helper'
 
 describe Administrate::Field::LatLng do
   
-  # FIXME!!
+  subject { described_class.new(field, 'lat_lng', :form) }
+  before(:each) do
+    allow(subject).to receive(:options).and_return(options)
+  end
+  
+  context 'lat' do
+    let(:field)   { :my_lat }
+    let(:options) { {lat: true} }
+    describe '#lat?' do
+      it { expect(subject.lat?).to be true }
+    end
+    describe '#lng?' do
+      it { expect(subject.lng?).to be false }
+    end
+    describe '#which' do
+      it { expect(subject.which).to eq(:lat) }
+    end
+  end
+  context 'lng' do
+    let(:field)   { :my_lng }
+    let(:options) { {lng: true} }
+    describe '#lat?' do
+      it { expect(subject.lat?).to be false }
+    end
+    describe '#lng?' do
+      it { expect(subject.lng?).to be true }
+    end
+    describe '#which' do
+      it { expect(subject.which).to eq(:lng) }
+    end
+    describe '#initial' do
+      it 'is default' do
+        expect(subject.initial).to eq([53.8003,-1.5519])
+      end
+    end
+    describe '#zoom' do
+      it 'is default' do
+        expect(subject.zoom).to eq(11)
+      end  
+    end
+    context 'with options' do
+      let(:options) { {lng: true, initial: [35.6809, 139.7672], zoom: 8} }
+      describe '#intial' do
+        it 'is overridden' do
+          expect(subject.initial).to eq([35.6809, 139.7672])
+        end
+      end
+      describe '#zoom' do
+        it 'is overridden' do
+          expect(subject.zoom).to eq(8)
+        end
+      end
+    end
+      
+  end
 
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,15 +1,2 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-require 'administrate/field/type'
-require 'active_record'
-
-class Example < ActiveRecord::Base
-end
-
-class One < Example
-end
-
-class Two < Example
-end
-
-class Three < Example
-end
+require 'administrate/field/lat_lng'
